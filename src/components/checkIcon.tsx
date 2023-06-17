f8e951c8b04d87e7ac9721746beb8d2f0caca851
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-17 13:18:01 +0200

    Animated CheckIcon

--- src/components/checkIcon.tsx
+++ src/components/checkIcon.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from "react";
+import { motion } from "framer-motion";
 
 function CheckIcon(props: ComponentProps<"svg">) {
   return (
@@ -9,7 +10,19 @@ function CheckIcon(props: ComponentProps<"svg">) {
       stroke="currentColor"
       strokeWidth={3}
     >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+      <motion.path
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1 }}
+        transition={{
+          delay: 0.2,
+          type: "tween",
+          ease: "easeOut",
+          duration: 0.3,
+        }}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5 13l4 4L19 7"
+      />
     </svg>
   );
 }
