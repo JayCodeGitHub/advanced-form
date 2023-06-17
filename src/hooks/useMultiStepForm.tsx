e4d915c2e46c99398d825c63cf89531997f3d737
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-17 22:35:47 +0200

    Refactored useMultiStepForm

--- src/hooks/useMultiStepForm.tsx
+++ src/hooks/useMultiStepForm.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
+const initialForm = {
+  firstName: "",
+  lastName: "",
+  phone: "",
+  email: "",
+  budget: "",
+  informations: "",
+};
+
 export function useMultiStepForm() {
-  const [form, setForm] = useState({
-    firstName: "",
-    lastName: "",
-    phone: "",
-    email: "",
-    budget: "",
-    informations: "",
-  });
+  const [form, setForm] = useState(initialForm);
   return { form, setForm };
 }
