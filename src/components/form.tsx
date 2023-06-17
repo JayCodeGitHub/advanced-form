2f2ac9e391014e01fc4dec4a62f73f8fba907efe
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-17 12:30:49 +0200

    Styled back button

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -8,11 +8,15 @@ function Form() {
         <div>4</div>
       </div>
       <div className="space-y-2 px-8">
-        <h1 className="text-black">Advanced Form</h1>
+        <div className="h-4 w-5/6 rounded bg-neutral-100" />
+        <div className="h-4 rounded bg-neutral-100" />
+        <div className="h-4 w-4/6 rounded bg-neutral-100" />
       </div>
       <div className="px-8 pb-8">
         <div className="mt-10 flex justify-between">
-          <button>Back</button>
+          <button className="duration-350 rounded px-2 py-1 text-neutral-400 transition hover:text-neutral-700">
+            Back
+          </button>
           <button>Continue</button>
         </div>
       </div>
