6b982631683378ba6605f3bd887bfd5f8232b678
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-17 12:23:48 +0200

    Styled form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,7 +1,21 @@
 function Form() {
   return (
-    <form>
-      <h1 className="text-white">Advanced Form</h1>
+    <form className="mx-auto w-full max-w-md rounded-lg bg-white shadow-xl">
+      <div className="flex justify-between rounded p-8">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+      <div className="space-y-2 px-8">
+        <h1 className="text-black">Advanced Form</h1>
+      </div>
+      <div className="px-8 pb-8">
+        <div className="mt-10 flex justify-between">
+          <button>Back</button>
+          <button>Continue</button>
+        </div>
+      </div>
     </form>
   );
 }
