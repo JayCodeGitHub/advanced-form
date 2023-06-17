40656ebe18bd1719abaf09ea87b9c0fa15a2b110
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-17 12:46:52 +0200

    Styled Step

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,13 +1,15 @@
 import Step from "./step";
+import { useState } from "react";
 
 function Form() {
+  let [step, setStep] = useState(1);
   return (
-    <form className="mx-auto w-full max-w-md rounded-lg bg-white shadow-xl">
+    <div className="mx-auto w-full max-w-md rounded-lg bg-white shadow-xl">
       <div className="flex justify-between rounded p-8">
-        <Step step={1} />
-        <Step step={2} />
-        <Step step={3} />
-        <Step step={4} />
+        <Step step={1} currentStep={step} />
+        <Step step={2} currentStep={step} />
+        <Step step={3} currentStep={step} />
+        <Step step={4} currentStep={step} />
       </div>
       <div className="space-y-2 px-8">
         <div className="h-4 w-5/6 rounded bg-neutral-100" />
@@ -16,15 +18,25 @@ function Form() {
       </div>
       <div className="px-8 pb-8">
         <div className="mt-10 flex justify-between">
-          <button className="duration-350 rounded px-2 py-1 text-neutral-400 transition hover:text-neutral-700">
+          <button
+            onClick={() => setStep(step < 2 ? step : step - 1)}
+            className={`${
+              step === 1 ? "pointer-events-none opacity-50" : ""
+            } duration-350 rounded px-2 py-1 text-neutral-400 transition hover:text-neutral-700`}
+          >
             Back
           </button>
-          <button className="bg duration-350 flex items-center justify-center rounded-full bg-blue-500 py-1.5 px-3.5 font-medium tracking-tight text-white transition hover:bg-blue-600 active:bg-blue-700">
+          <button
+            onClick={() => setStep(step > 4 ? step : step + 1)}
+            className={`${
+              step > 4 ? "pointer-events-none opacity-50" : ""
+            } bg duration-350 flex items-center justify-center rounded-full bg-blue-500 py-1.5 px-3.5 font-medium tracking-tight text-white transition hover:bg-blue-600 active:bg-blue-700`}
+          >
             Continue
           </button>
         </div>
       </div>
-    </form>
+    </div>
   );
 }
 
--- src/components/step.tsx
+++ src/components/step.tsx
@@ -1,11 +1,28 @@
 interface StepProps {
   step: number;
+  currentStep: number;
 }
 
-function Step({ step }: StepProps) {
+function Step({ step, currentStep }: StepProps) {
+  let status =
+    currentStep === step
+      ? "active"
+      : currentStep < step
+      ? "inactive"
+      : "complete";
   return (
-    <div>
-      <span>{step}</span>
+    <div
+      className={`${
+        status === "active"
+          ? "border-blue-500 bg-white text-blue-500"
+          : status === "complete"
+          ? "border-blue-500 bg-blue-500 text-white"
+          : "border-slate-200 bg-white text-slate-400"
+      } flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold`}
+    >
+      <div className="flex items-center justify-center">
+        <span>{step}</span>
+      </div>
     </div>
   );
 }
