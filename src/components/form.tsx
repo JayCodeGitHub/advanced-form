d31a31db9ddc45eb1f9a0c44a74dfe20ab8db100
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-17 13:09:55 +0200

    Added Dynamic content based on

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,6 @@
-import Step from "./step";
 import { useState } from "react";
+import { motion } from "framer-motion";
+import Step from "./step";
 
 function Form() {
   let [step, setStep] = useState(1);
@@ -12,9 +13,37 @@ function Form() {
         <Step step={4} currentStep={step} />
       </div>
       <div className="space-y-2 px-8">
-        <div className="h-4 w-5/6 rounded bg-neutral-100" />
-        <div className="h-4 rounded bg-neutral-100" />
-        <div className="h-4 w-4/6 rounded bg-neutral-100" />
+        {step === 1 ? (
+          <>
+            <div className="h-4 w-5/6 rounded bg-neutral-100" />
+          </>
+        ) : step === 2 ? (
+          <>
+            <div className="h-4 w-5/6 rounded bg-neutral-100" />
+            <div className="h-4 rounded bg-neutral-100" />
+          </>
+        ) : step === 3 ? (
+          <>
+            <div className="h-4 w-5/6 rounded bg-neutral-100" />
+            <div className="h-4 rounded bg-neutral-100" />
+            <div className="h-4 w-4/6 rounded bg-neutral-100" />
+          </>
+        ) : step === 4 ? (
+          <>
+            <div className="h-4 w-5/6 rounded bg-neutral-100" />
+            <div className="h-4 rounded bg-neutral-100" />
+            <div className="h-4 w-4/6 rounded bg-neutral-100" />
+            <div className="h-4 w-4/6 rounded bg-neutral-100" />
+          </>
+        ) : step === 5 ? (
+          <>
+            <div className="h-4 w-5/6 rounded bg-neutral-100" />
+            <div className="h-4 rounded bg-neutral-100" />
+            <div className="h-4 w-4/6 rounded bg-neutral-100" />
+            <div className="h-4 w-4/6 rounded bg-neutral-100" />
+            <div className="h-4 w-4/6 rounded bg-neutral-100" />
+          </>
+        ) : null}
       </div>
       <div className="px-8 pb-8">
         <div className="mt-10 flex justify-between">
@@ -27,13 +56,13 @@ function Form() {
           >
             Back
           </button>
-          <button
+          <motion.button
             onClick={() => setStep(step > 5 ? step : step + 1)}
             type={step > 5 ? "submit" : "button"}
             className="bg duration-350 flex items-center justify-center rounded-full bg-blue-500 py-1.5 px-3.5 font-medium tracking-tight text-white transition hover:bg-blue-600 active:bg-blue-700"
           >
             {step > 4 ? "Submit" : "Continue"}
-          </button>
+          </motion.button>
         </div>
       </div>
     </form>
